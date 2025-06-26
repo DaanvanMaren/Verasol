cf029cf839e04fc9ad4cd1624b64102aba27a44b
Daan v m <Daan2003@outlook.com>
2025-06-26 10:42:40 +0200

    uitgezet, moet hier later op terugkomen

--- dist/script-verasol.dev.js
+++ dist/script-verasol.dev.js
@@ -13,23 +13,22 @@ function updateVisibility() {
 
     shapeBg.style.display = hasActie ? '' : 'none';
   });
-}
-
-function updateVisibilityTwee() {
-  var hasActie = !!document.querySelector('.actie-categorie'); // 1) USP togglen
-
-  document.querySelectorAll('.usp2').forEach(function (el) {
-    el.style.display = hasActie ? 'none' : '';
-  }); // 2) x-shape-bg onder .actie-vaantje-onderkant togglen
-
-  document.querySelectorAll('.actie-vaantje-onderkant').forEach(function (container) {
-    var shapeBg = container.querySelector('x-shape-bg');
-    if (!shapeBg) return; // als er géén .actie is, verberg; anders toon
-
-    shapeBg.style.display = hasActie ? '' : 'none';
-  });
 } // bij paginaload
 
 
-document.addEventListener('DOMContentLoaded', updateVisibility);
-document.addEventListener('DOMContentLoaded', updateVisibilityTwee);
+document.addEventListener('DOMContentLoaded', updateVisibility); // function updateVisibilityTwee() {
+//     const hasActie = !!document.querySelector('.actie-categorie');
+//     // 1) USP togglen
+//     document.querySelectorAll('.usp').forEach(el => {
+//       el.style.display = hasActie ? 'none' : '';
+//     });
+//     // 2) x-shape-bg onder .actie-vaantje-onderkant togglen
+//     document.querySelectorAll('.actie-vaantje-onderkant').forEach(container => {
+//       const shapeBg = container.querySelector('x-shape-bg');
+//       if (!shapeBg) return;
+//       // als er géén .actie is, verberg; anders toon
+//       shapeBg.style.display = hasActie ? '' : 'none';
+//     });
+//   }
+//   // bij paginaload
+//   document.addEventListener('DOMContentLoaded', updateVisibilityTwee);
